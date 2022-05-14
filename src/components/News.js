929577e757049c58fc67dd094c3fc9e5bb0e05a2
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-05-14 13:17:05 +0530

    added the infinite scroll barwq

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from '../NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
 
@@ -24,24 +25,26 @@ export default class News extends Component {
             articles: [],
             loading: false,
             page: 1,
-            category: 'general'
+            category: 'general',
+            totalResults: 0
         }
     }
 
     // runs at the last 
     async componentDidMount(eee) {
         console.log("cdm");
-        this.updateNews() ; 
+        this.updateNews();
     }
 
     async updateNews() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey={API_KEY}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bc85e742959d47138fc775410fde6e1f&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         fetch(url).then((res) => res.json())
             .then((json) => {
                 this.setState({
                     articles: json.articles,
-                    loading: false
+                    loading: false,
+                    totalResults: json.totalResults
                 });
             })
     }
@@ -59,25 +62,44 @@ export default class News extends Component {
         this.updateNews();
     }
 
-
+    // fetches more data 
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1}) ; 
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey={API_KEY}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        fetch(url).then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    articles: this.state.articles.concat(json.articles),
+                    loading: false,
+                    totalResults: json.totalResults
+                });
+            })
+    };
 
     render() {
         return (
             <div className='container my-3'>
                 <h1 className='text-center'>Top Headlines</h1>
                 {this.state.loading && <Spinner />}
-                <div className="row my-3">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
-                        </div>
-                    })
-                    }
-                </div>
-                <div className="container d-flex justify-content-between">
-                    <button disabled={this.state.page <= 1} type="button" className="btn btn-secondary" onClick={this.handlePrevPage}>&larr;Previous</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-secondary" onClick={this.handleNextPage}>Next &rarr;</button>
-                </div>
+
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<h3><Spinner /></h3>}
+                >
+                    
+                    <div className="row my-3 container">
+                        {this.state.articles.map((element ,index) => {
+                            return <div className="col-md-4" key={index}>
+                                <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
+                            </div>
+                        })
+                        }
+                    </div>
+                </InfiniteScroll>
+
             </div>
         )
     }
